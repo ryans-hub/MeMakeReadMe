d033e70ab62a69e0607ec55dadc94eab9b3c0e18
PanMan <ryanispan@gmail.com>
2024-02-29 20:38:42 -0700

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,38 @@ const inquire = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project called?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Describe your project.',
+        name: 'projectDesc',
+    },
+    {
+        type: 'input',
+        message: 'Installation instructions?',
+        name: 'projectIns',
+    },
+    {
+        type: 'input',
+        message: 'Usage information?',
+        name: 'projectUsage',
+    },
+    {
+        type: 'input',
+        message: 'Contribution guidelines?',
+        name: 'projectContr',
+    },
+    {
+        type: 'input',
+        message: 'Test Instructions?',
+        name: 'test',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
